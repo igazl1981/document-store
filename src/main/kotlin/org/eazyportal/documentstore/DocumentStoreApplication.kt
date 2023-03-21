4b3b5b83ed76ec3c1aead62dcdf48afade21222e
Yg0R2 <yg0r2@yahoo.com>
2023-03-21 11:34:06 +0100

    fix: upgrade eazyporta-core version to 0.8.3

--- src/main/kotlin/org/eazyportal/documentstore/DocumentStoreApplication.kt
+++ src/main/kotlin/org/eazyportal/documentstore/DocumentStoreApplication.kt
@@ -2,7 +2,14 @@ package org.eazyportal.documentstore
 
 import org.eazyportal.core.extension.runEazyPortalApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.cloud.openfeign.EnableFeignClients
 
+@EnableFeignClients(
+    basePackages = [
+        "org.eazyportal.core",
+        "org.eazyportal.documentstore"
+    ]
+)
 @SpringBootApplication
 class DocumentStoreApplication
 
